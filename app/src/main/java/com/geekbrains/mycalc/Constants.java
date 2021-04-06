9c40f6b4ccfe2d291d8c934c5b154b7b40667af3
Kirill <radysh@inbox.ru>
2021-04-06 18:01:21 +0300

    Home Work 5

---
+++ app/src/main/java/com/geekbrains/mycalc/Constants.java
@@ -0,0 +1,27 @@
+package com.geekbrains.mycalc;
+
+public class Constants {
+    public static final String mainActivityDataKey = "mainActivityDataKey";
+    public static final String changeThemeActivityDataKey = "changeThemeActivityDataKey";
+    public static final String calculatorStateKey = "calculatorStateKey";
+    public static final String codeThemeKey = "codeThemeKey";
+    public static final int changeThemeActivityRequestCode = 9876;
+
+    public static final int darkTheme = 2;
+    public static final int lightTheme = 1;
+    public static final int skyTheme = 0;
+
+    public static final char PERCENT = '%';
+    public static final char SUMMATION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+    public static final char EMPTY = ' ';
+
+    // Имя настроек
+    public static final String NameSharedPreference = "CALC";
+
+    // Имя параметра в настройках
+    public static final String appTheme = "APP_THEME";
+
+}
